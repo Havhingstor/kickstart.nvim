8c04ee580aaa843dd0d9d97fbca37ca3567d691c
Paul Schuetz <pa.schuetz@web.de>
2025-11-02 13:47:24 +0100

    Move the jdtls setup into lspconfig

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -216,6 +216,12 @@ return {
       --  2) via your system's package manager; or
       --  3) via a release binary from a language server's repo that's accessible somewhere on your system.
 
+      local get_workspace_name = function()
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+
+        return '/Users/paul/Programmieren/Java/jdtls-data/' .. project_name
+      end
+
       -- The servers table comprises of the following sub-tables:
       -- 1. mason
       -- 2. others
@@ -277,6 +283,37 @@ return {
           -- dartls = {},
           sourcekit = {
             cmd = { vim.trim(vim.fn.system 'xcrun -f sourcekit-lsp') },
+          },
+          jdtls = {
+            -- `cmd` defines the executable to launch eclipse.jdt.ls.
+            -- `jdtls` must be available in $PATH and you must have Python3.9 for this to work.
+            --
+            -- As alternative you could also avoid the `jdtls` wrapper and launch
+            -- eclipse.jdt.ls via the `java` executable
+            -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+            cmd = { 'jdtls', '-data', get_workspace_name() },
+
+            -- `root_dir` must point to the root of your project.
+            -- See `:help vim.fs.root`
+            root_dir = vim.fs.root(0, { 'gradlew', '.git', 'mvnw' }),
+
+            -- Here you can configure eclipse.jdt.ls specific settings
+            -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+            -- for a list of options
+            settings = {
+              java = {},
+            },
+
+            -- This sets the `initializationOptions` sent to the language server
+            -- If you plan on using additional eclipse.jdt.ls plugins like java-debug
+            -- you'll need to set the `bundles`
+            --
+            -- See https://codeberg.org/mfussenegger/nvim-jdtls#java-debug-installation
+            --
+            -- If you don't plan on any eclipse.jdt.ls plugins you can remove this
+            init_options = {
+              bundles = {},
+            },
           },
         },
       }
